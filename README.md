95d589040cfdf702ed9907e873ef3ec48051824a
Rosyming <41979276+rosyming@users.noreply.github.com>
2018-10-23 22:52:53 -0400

    Final working App

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# liri-node-app
-GATech Coding Bootcamp Homework: liri-node-app
+## liri-node-app
+### GATech Coding Bootcamp Homework: liri-node-app
+Updated: 10/23/18
+
+- - -
+### Overview
+Developed an application called LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives data back to the user. 
+
+LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.
+
+- - -
+### How to Operate the App
+1. Start node in bash terminal referencing the liri.js file (node liri.js)
+
+2. Enter in one of these commands
+    * concert-this <artist/band name here>
+    * spotify-this-song <song name here>
+    * movie-this <movie name here>
+    * do-what-it-says
+
+3. After the command enter in a search term related to the command. For example:
+    * concert-this Lady Gaga
+    * spotify-this-song Somewhere Over the Rainbow
+    * movie-this Moonrise Kingdom
+    * do-what-it-says (Enter in text in the random.txt file)
+
+4. Hit return. Results will be displayed as described below:
+    * concert-this will search the Bands in Town Artist Events API for the artist and will display the information for up to 5 events
+    * spotify-this-song will show search the Spotify API for a the song and will display information for up to 5 songs sharing the same title. If no song is provided then the default song would be "The Sign" by Ace of Base.
+    * movie-this will search the OMDB API for the movie and will display information about the movie. If no movie is provided then the default movie would be Mr. Nobody.
+    * do-what-it-says will take command and search term in the random.txt file and initiate the search based upon what is written in the file for the commands listed in #3 above. The random.txt file can be altered, but only one text line should be present.
+
+- - -
+### Video of Working App
+http://recordit.co/R0MFky3AVE
+
+- - -
+### Technologies Used
+1. Node.js
+2. NPMs and APIs: Node-Spotify-API, Request, OMDB API, Bands In Town API, Moment, DotEnv
+
+- - -
+### Repository
+* https://github.com/rosyming/liri-node-app
+
+
+
+
+
--- liri.js
+++ liri.js
@@ -1,114 +1,126 @@
 // Requiring the dotenv for the spotify keys
 (function main() {
     require('dotenv').config();
-    console.log('SPOTIFY_ID: ' + process.env.SPOTIFY_ID);
-    console.log('SPOTIFY_SECRET: ' + process.env.SPOTIFY_SECRET);
 }());
 
 // Requirng to let us access all key.js exports
 var keys = require('./keys.js');
-console.log('-----------');
-console.log('keys: ' + keys.spotify.id);
 
-// Takeing two arguments.
-// The first will be the action (i.e., 'concert-this', 'spotify-this-song', etc.)
+// Takeing two arguments. The first will be the action (i.e., 'concert-this', 'spotify-this-song', etc.)
 // The second will be the input for the action (i.e., 'song name', 'artist/band name', etc. )
 var nodeArgs = process.argv;
 var action = nodeArgs[2];
-var value = nodeArgs[3];
+var value = nodeArgs.slice(3).join(' ');
 
-// Creating switch-case to direct which function will run.
-switch (action) {
-    case 'concert-this':
-        concertSearch();
-        break;
+// Creating switch-case to direct which function will run========================
+function Switching(action, value) {
+    switch (action) {
+        case 'concert-this':
+            concertSearch(value);
+            break;
 
-    case 'spotify-this-song':
-        spotifySearch();
-        break;
+        case 'spotify-this-song':
+            spotifySearch(value);
+            break;
 
-    case 'movie-this':
-        movieSearch();
-        break;
+        case 'movie-this':
+            movieSearch(value);
+            break;
 
-    case 'do-what-it-says':
-        doThis();
-        break;
-}
+        case 'do-what-it-says':
+            doThis();
+            break;
+    };
+};
 
-// Concert Search Function
-function concertSearch() {
+// Concert Search Function============================================
+function concertSearch(value) {
     var request = require('request');
-    var bandName = '';
+    var moment = require('moment');
 
-    // Allowing for mult-word searches
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            bandName = bandName + '+' + nodeArgs[i];
-        } else {
-            bandName += nodeArgs[i];
-        };
+    // If no band is provided, default band is Pentatonix
+    if (!value) {
+        value = 'Pentatonix';
     };
 
     // API HTTP Request
     var bandQueryUrl = 'https://rest.bandsintown.com/artists/' + value + '/events?app_id=codingbootcamp';
     
     request(bandQueryUrl, function (error, response, body) {
-    
+
         // If the request is successful (i.e. if the response status code is 200)
         if (!error && response.statusCode === 200) {
+            var bandQueryArr = [];
+            bandQueryArr = JSON.parse(body);
+            
+            // Loop through request response for each venue and display info from response (e.g., Name of Venue, Venue location, Date of event)
+            for (k = 0; k < 5; k++) {
+                console.log('-------------');
+                console.log('Upcoming concerts for ' + value + ':')
+                console.log('Name of Venue: ' + bandQueryArr[k].venue.name);
+                console.log('Venue Location: ' + bandQueryArr[k].venue.city + ', ' + bandQueryArr[k].venue.region + ', ' + bandQueryArr[k].venue.country);
 
-            // Parse the body of the site and recover Title, Year, imdbRating, Rotton Tomatoes Rating, Country, Language, Plot, and Actors
-            console.log('-------------');
-            console.log('Upcoming concerts for ' + value + ':')
-            console.log('The artist name: ' + JSON.parse(body).id);
-            console.log('-------------');
-        }
+                // Using Moment.js npm to display formatted date
+                var randomDate = bandQueryArr[k].datetime;
+                console.log('Date of Event: ' + 
+                moment(randomDate).format('MM/DD/YYYY')             
+                );
+            };   
+        };
     });
 };
 
-// Spotify Search Function
-function spotifySearch() {
+// Spotify Search Function============================================
+function spotifySearch(value) {
     var Spotify = require('node-spotify-api');
-
+    var returnLimit = 5
+    
     var spotify = new Spotify({
         id: keys.spotify.id,
         secret: keys.spotify.secret
     });
 
+    // If no song is provided, default song is "The Sign" by Ace of Base
+    if (!value) {
+        value = 'The Sign Ace of Base';
+        returnLimit = 1;
+    };
+
     spotify.search({
         type: 'track',
         query: value,
-        limit: 10, 
-        callback
+        limit: returnLimit,
     }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
+        };
 
-        console.log('My spotify song:' + JSON.parse(data));
+        // Loop through request response for each song and display info from response (e.g., Artist name, Song name, Preview link, and Album)
+        for (j = 0; j < data.tracks.items.length; j++) {
+            var tracks = data.tracks
+            console.log('-------------');
+            console.log('Artist(s): ' + tracks.items[j].artists[0].name);
+            console.log('Song Name: ' + tracks.items[j].name);
+            console.log('Spotify Preview Link: ' + tracks.items[j].preview_url);
+            console.log('Album: ' + tracks.items[j].album.name);
+        };
     });
 };
 
-// OMDB Search Function
-function movieSearch() {
+// OMDB Search Function===============================================
+function movieSearch(value) {
     var request = require('request');
-    var movieName = '';
 
-    // Allowing for mult-word searches
-    for (var i = 3; i < nodeArgs.length; i++) {
-        if (i > 3 && i < nodeArgs.length) {
-            movieName = movieName + '+' + nodeArgs[i];
-        } else {
-            movieName += nodeArgs[i];
-        };
+    // If no movie is provided, default movie is "Mr. Nobody"
+    if (!value) {
+        value = 'Mr. Nobody';
+        console.log('-------------');
+        console.log('If you haven\'t watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>');
+        console.log('It\'s on Netflix!');
     };
-
+     
     // API HTTP Request
     var movieQueryUrl = 'http://www.omdbapi.com/?t=' + value + '&y=&plot=short&apikey=trilogy';
-    var movieQueryUrl2 = 'http://www.omdbapi.com/?t=Mr.Nobody&y=&plot=short&apikey=trilogy';
-
-    console.log(movieQueryUrl);
 
     request(movieQueryUrl, function (error, response, body) {
 
@@ -125,29 +137,32 @@ function movieSearch() {
             console.log('Language: ' + JSON.parse(body).Language);
             console.log('Plot: ' + JSON.parse(body).Plot);
             console.log('Actors: ' + JSON.parse(body).Actors);
-            console.log('-------------');
         };
-
-        if (value === '')
-            return request(movieQueryUrl2, function (error, response, body) {
-                // If the request is successful
-                if (!error && response.statusCode === 200) {
-
-                // Parse the body of the site and recover Title, Year, imdbRating, Rotton Tomatoes Rating, Country, Language, Plot, and Actors
-                console.log('-------------');
-                console.log('Movie Title: ' + JSON.parse(body).Title);
-                console.log('Release Year: ' + JSON.parse(body).Year);
-                console.log('IMDB Rating: ' + JSON.parse(body).imdbRating);
-                console.log('Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value);
-                console.log('Country Produced: ' + JSON.parse(body).Country);
-                console.log('Language: ' + JSON.parse(body).Language);
-                console.log('Plot: ' + JSON.parse(body).Plot);
-                console.log('Actors: ' + JSON.parse(body).Actors);
-                console.log('-------------');
-                console.log('If you haven\'t watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>');
-                console.log('It\'s on Netflix!');
-                console.log('-------------');
-            };
-        });
     });
 };
+
+// OMDB Search Function===============================================
+function doThis (value) {
+    var fs = require("fs");
+
+    // Reading the random.txt file
+    fs.readFile("random.txt", "utf8", function(err, data) {
+        if (err) {
+          return console.log(err);
+        }
+
+        data = data.split(',');
+        action = data[0];
+        value = data[1].replace(/['"]+/g, '')  
+        
+        Switching(action, value);
+    });
+};
+
+// Starting Switch Function
+Switching(action, value);
+
+
+
+
+
